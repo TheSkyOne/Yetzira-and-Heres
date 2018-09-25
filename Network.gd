d3b8f27158afb117610a5775d1e45a34df607abf
omer <omer9955821@gmail.com>
2018-09-25 21:15:35 +0300

    game starts on both screens when the 2nd player is connected

--- Network.gd
+++ Network.gd
@@ -3,8 +3,11 @@ extends Node
 const SERVER_PORT = 3297
 const SERVER_IP = "127.0.0.1"
 const MAX_PLAYERS = 2
+var player_info = {name = "", role = ""}
 var amount_connected = 0
-var player_info = {name = ""}
+
+func _process(delta):
+	print(amount_connected)
 
 func _ready():
 	get_tree().connect("network_peer_connected", self, "player_connected")
@@ -13,9 +16,6 @@ func _ready():
 	get_tree().connect("connection_failed", self, "connected_fail")
 	get_tree().connect("server_disconnected", self, "server_disconnected")
 
-func _process(delta):
-	print(amount_connected)
-	
 func create_server():
 	var host = NetworkedMultiplayerENet.new()
 	host.create_server(SERVER_PORT, MAX_PLAYERS)
@@ -26,12 +26,19 @@ func connect_to_server():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_client(SERVER_IP, SERVER_PORT)
 	get_tree().set_network_peer(peer)
-	
 
 func player_connected(id):
-	player_info[id] = id
-	amount_connected += 1
-
+	player_info.name = id
+	if is_network_master():
+		amount_connected += 1
+	else: 
+		amount_connected += 2
+	var x = rand()%0+1
+	if x == 0:
+		player_info.role = "Yetzira"
+	else:
+		player_info.role = "Heres"
+	
 func player_disconnected(id):
     player_info.erase(id)
 	
@@ -51,7 +58,4 @@ remote func register_player(id, info):
 	    rpc_id(id, "register_player", 1, player_info)
 	    for peer_id in player_info:
 	        rpc_id(id, "register_player", peer_id, player_info[peer_id])
-
-
-
 
